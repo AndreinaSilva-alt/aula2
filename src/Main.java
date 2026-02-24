9804c8b9d98e8a7806e23cbfff8465326c75c58f
AndreinaSilva-alt <andreinasilva471@gmail.com>
2026-02-23 21:46:29 -0400

    git add

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+ class Aluno {
+
+        String nome;
+        String dataDeNascimento;
+        int idade;
+    }
+
+    public class Main{
+        public static void main(String[] args){
+             Aluno aluno = new Aluno();
+
+             aluno.nome = "Kamila";
+             aluno.dataDeNascimento = "13/07/2004";
+             aluno.idade = 21;
+
+             System.out.println("A aluna é "+ aluno.nome + " que nasceu em " + aluno.dataDeNascimento +  " tem " + aluno.idade + " anos.") ;
+        }
+    }
+
